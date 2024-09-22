9f2b7acba0b0ea163d7c3df1a797c6c46293588c
gull2112 <163505052+gull2112@users.noreply.github.com>
2024-09-22 15:26:36 +0900

    3주차 퀴즈
    3주차 퀴즈

---
+++ 3주차 퀴즈.py
@@ -0,0 +1,14 @@
+#1
+a = "Hello, World!"
+print(a[0:5])
+#2
+b=7+3
+result=b**2
+print(result)
+#3
+A=[1,2,3,4,5]
+result=[A[4],A[3],A[2],A[1],A[0]]
+print(result)
+#4
+dic={'apple': 3, 'banana': 5, 'cherry': 2}
+print(dic['banana'])
